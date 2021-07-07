4344b7ad8338f7d966a9ebc7470800020a496a9e
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-07-07 19:50:06 -0300

    void type

--- src/A001-type-annotation/A003-type-void.ts
+++ src/A001-type-annotation/A003-type-void.ts
@@ -2,4 +2,12 @@
 function showNames(...names: string[]): void {
    console.log(names.join(", "))
 }
+const pessoa = {
+   nome: "VICTOR OBJETO",
+   idade: 27,
+   showName(): void {
+      console.log(this.nome)
+   }
+}
 showNames("victor", "joão")
+pessoa.showName()

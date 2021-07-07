e723e5644adf3cc2c3f9b843b8f0a783125df7c5
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-07-07 19:26:44 -0300

    type anotation

--- src/A001-type-annotation/A000-type-annotation.ts
+++ src/A001-type-annotation/A000-type-annotation.ts
@@ -5,10 +5,25 @@ let adulto: boolean = false; // true ou false
 let símbolo: symbol; // qualquer símbolo
 // let big: bigint =10n //bigint
 
-// let arrayOfNumbers: Array<number> = [1, 3, 4, 5];
+let arrayOfNumbers2: Array<number> = [1, 3, 4, 5];
 // ou pode declarar
 let arrayOfNumbers: number[] = [1, 3, 4, 5];
 
 // let arrayOfNumbersStrings: Array<number | string> = [1, '4', 3, 4, 5];
 // ou pode declarar
-let arrayOfNumbersStrings: string[] = [1, '4', 3, 4, 5];
+
+let arrayOfNumbersStrings: any[] = [1, '4', 3, 4, 5];
+
+
+// objetos
+// O ?: define uma propriedade não obrigatória , adulto aceita undefined
+let pessoas: { nome: string, idade: number, adulto?: boolean } = { nome: "Victor", idade: 99, }
+
+// funcoes
+// pode definir assim
+function soma(x: number, y: number): number {
+   return x + y
+}
+// ou
+const soma: (a: number, b: number) => number = (a, b) => a + b
+const result = soma(1, 2)

94252057d4b203427a2b98c1ff511106b101d596
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-07-13 20:22:02 -0300

    union types

---
+++ src/tipos-basicos/A011-union-types.ts
@@ -0,0 +1,7 @@
+export function addOrConcat(a:number | string, b:number| string):number|string{
+   if(typeof a ==="number" && typeof b ==="number")return a + b
+   return `${a}${b}`
+}
+console.log(addOrConcat(20,30))
+console.log(addOrConcat("victor ","magaldi"))
+

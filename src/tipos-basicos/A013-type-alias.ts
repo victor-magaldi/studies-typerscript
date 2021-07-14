2d1b96a0d83b0d05c33ba9dd4df2f1b76bfe534a
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-07-14 20:01:36 -0300

    trabalhando com type alias

--- src/tipos-basicos/A013-type-alias.ts
+++ src/tipos-basicos/A013-type-alias.ts
@@ -1,5 +1,9 @@
-type Color= "red"| "green"| "white " ;
+type ColorRGB= "red"| "green"| "azul " ;
+type ColorCMYK= "ciano"| "magenta"| "amarelo " |"amarelo";
 
-const color:Color= "red"
+type allColor = ColorRGB|ColorCMYK
+const color:allColor= "magenta"
 
+
+console.log(color)
 export default 1
